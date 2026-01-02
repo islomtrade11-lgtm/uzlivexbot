7bc96277e23effdb22a185f04b03ee17fae24323
islomtrade11-lgtm <islomtrade11@gmail.com>
2026-01-02 22:39:18 +0500

    Load environment variables from specific path

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()
+from pathlib import Path
+
+env_path = Path(__file__).resolve().parent / ".env"
+load_dotenv(dotenv_path=env_path)
 
 import sqlite3
 import requests
